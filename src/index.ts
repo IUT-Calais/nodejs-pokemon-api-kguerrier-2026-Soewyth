305e06a234018e512e67981289744b3a3f1b3afa
Dhesdin <valentin_dhs@hotmail.com>
2026-02-06 11:18:21 +0100

    adding router pokemons-attacks

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import { pokemonCardRouter } from './pokemonCard/pokemonCard.router';
+import { pokemonAttackRouter } from './pokemonAttack/pokemonAttack.router';
 import { userRouter } from './user/user.router';
 export const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// routers for different resources
 app.use('/pokemons-cards', pokemonCardRouter);
+app.use('/pokemons-attacks', pokemonAttackRouter);
 app.use('/users', userRouter);
 
 export const server = app.listen(port);
