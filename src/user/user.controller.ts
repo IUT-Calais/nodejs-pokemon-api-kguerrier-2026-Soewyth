21cd385639b8e868e2e53f6b6fa0ac001e9577d5
Dhesdin <valentin_dhs@hotmail.com>
2026-02-06 14:25:17 +0100

    debug jwt expire

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -93,7 +93,7 @@ export async function loginUser(req: Request, res: Response) {
         const token = jwt.sign(
             { userId: user.id, email: user.email },
             process.env.JWT_SECRET as string,
-            { expiresIn: '2'}
+            { expiresIn: '2h'}
         );
 
         // Retourner le token
