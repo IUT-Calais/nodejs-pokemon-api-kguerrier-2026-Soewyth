476bf95d18c901266bbf19bd9abe700147566e05
Dhesdin <valentin_dhs@hotmail.com>
2026-02-14 08:28:27 +0100

    creating swagger(docs) file

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,13 @@ import { pokemonCardRouter } from './pokemonCard/pokemonCard.router';
 import { pokemonAttackRouter } from './pokemonAttack/pokemonAttack.router';
 import { userRouter } from './user/user.router';
 import { pokemonDeckRouter } from './deck/deck.router';
+// On ajoute les informations pour swagger
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
+
 export const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(express.json());
-
-// routers for different resources
-app.use('/pokemon-cards', pokemonCardRouter);
-app.use('/pokemon-attacks', pokemonAttackRouter);
-app.use('/users', userRouter);
-app.use('/decks', pokemonDeckRouter);
 
 let server: ReturnType<typeof app.listen> | null = null;
 
@@ -21,6 +18,20 @@ if (require.main === module) {
   server = app.listen(port);
   console.log(`Le serveur est démarré sur le port :  '${port}'`);
 }
+
+// On charge la spécification Swagger
+const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
+
+// Et on affecte le Serveur Swagger UI à l'adresse /api-docs
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use(express.json());
+
+// routers for different resources
+app.use('/pokemon-cards', pokemonCardRouter);
+app.use('/pokemon-attacks', pokemonAttackRouter);
+app.use('/users', userRouter);
+app.use('/decks', pokemonDeckRouter);
 
 export function stopServer() {
   if (server) {
